8038939603d4e3561824c894e9ae058d46b7d529
nguyentuanhung123 <nguyentuanhung4529871036@gmail.com>
2024-01-26 23:41:30 +0700

    add server on api and RegisterPage in client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import {Navigate, Route, Routes} from "react-router-dom";
 import IndexPage from './pages/IndexPage';
 import LoginPage from './pages/LoginPage';
 import Layout from './components/Layout';
+import RegisterPage from './pages/RegisterPage';
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <Route path='/' element={<Layout />}>
         <Route index element={<IndexPage />}></Route>
         <Route path='/login' element={<LoginPage />}></Route>
+        <Route path='/register' element={<RegisterPage />}></Route>
         <Route path='*' element={<Navigate to="/"/>}></Route>
       </Route>
     </Routes>
--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,21 +1,21 @@
 import { Link } from "react-router-dom";
 
-const LoginPage = () => {
+const RegisterPage = () => {
     return(
         <div className="mt-4 grow flex items-center justify-around">
             <div className="mb-64">
-                <h1 className="text-4xl text-center mb-4">Login</h1>
+                <h1 className="text-4xl text-center mb-4">Register</h1>
                 <form className="max-w-md mx-auto">
+                    <input type="text" placeholder="Nguyen Tuan Hung"/>
                     <input type="email" placeholder="your@email.com"/>
                     <input type="password" placeholder="password"/>
                     <button className="primary">Login</button>
                     <div className="text-center py-2 text-gray-500">
-                        Don&#39;t have an account yet? <Link className="underline text-black" to='/register'>Register now</Link>
+                        Already a member? <Link className="underline text-black" to='/login'>Login</Link>
                     </div>
                 </form>
             </div>
         </div>
     )
 }
-
-export default LoginPage;
+export default RegisterPage;
