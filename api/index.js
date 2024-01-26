311ef78d85a54dcfae022895ecfe19e3c7f8c11c
nguyentuanhung123 <nguyentuanhung4529871036@gmail.com>
2024-01-27 00:20:17 +0700

    register on axios

--- api/index.js
+++ api/index.js
@@ -1,9 +1,28 @@
 const express = require('express')
+const cors = require('cors');
 const app = express()
 const port = 4000
 
+//ta cần phân tích cú pháp json
+app.use(express.json());
+
+// link to client
+app.use(cors({
+  credentials: true,
+  origin: 'http://localhost:5200',
+}))
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/test', (req, res) => {
+  res.send('test ok!')
+})
+
+app.post('/register', (req, res) => {
+  const { name, email, password } = req.body;
+  res.json({name, email, password});
 })
 
 
