efa1bd5f46fcee46ab04e904347bd9f8363b37de
nguyentuanhung123 <nguyentuanhung4529871036@gmail.com>
2024-02-04 08:43:36 +0700

    edit ProfilePage.jsx

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -2,33 +2,33 @@ import { useContext, useState } from "react";
 import { UserContext } from "../context/UserContext";
 import { Navigate, useParams } from "react-router-dom";
 import axios from "axios";
-import PlacesPage from "./PlacesPage";
+//import PlacesPage from "./PlacesPage";
 import AccountNav from "../components/AccountNav";
 
 const ProfilePage = () => {
 
-    let { subpage } = useParams();
+    //let { subpage } = useParams();
 
     const {ready, user, setUser} = useContext(UserContext);
 
     const [redirect, setRedirect] = useState(null);
 
-    if(subpage === undefined){
-        subpage = 'profile'
-    }
+    // if(subpage === undefined){
+    //     subpage = 'profile'
+    // }
 
     if(ready && !user){
         return <Navigate to={'/login'} />
     }
-    // // bug : Khi ta refresh thì nó sẽ chuyển đến trang login dù ta đang ở account page 
-    // // (Lý do ở context ta đặt user mặc đinh là null và phải mất vài mili giây để cập lại user)
-    // // Giải pháp bổ sung ready
+    // bug : Khi ta refresh thì nó sẽ chuyển đến trang login dù ta đang ở account page 
+    // (Lý do ở context ta đặt user mặc đinh là null và phải mất vài mili giây để cập lại user)
+    // Giải pháp bổ sung ready
 
     if(!ready){
         return 'Loading...';
     }
 
-    // //console.log("Subpage : ", subpage); mặc định là undifined
+    //console.log("Subpage : ", subpage); //mặc định là undifined
 
     const logout = async () => {
         await axios.post('/logout');
@@ -46,7 +46,7 @@ const ProfilePage = () => {
         // <div>account page for {user.name}</div>
         <div>
             <AccountNav />
-            {
+            {/* {
                 subpage === 'profile' && 
                     <div className="text-center max-w-lg mx-auto">
                         Logged in as a {user.name} ({user.email})
@@ -55,7 +55,11 @@ const ProfilePage = () => {
             }
             {
                 subpage === 'places' && <PlacesPage />
-            }
+            } */}
+            <div className="text-center max-w-lg mx-auto">
+                Logged in as a {user.name} ({user.email})
+                <button onClick={logout} className="primary max-w-sm mt-2">Logout</button>
+            </div> 
         </div>
     )
 }
